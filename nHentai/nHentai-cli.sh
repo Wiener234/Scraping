726c56cb7a17b422640d7eedfcff1d142f49578b
Wiener234 <wiemernils@gmail.com>
2022-05-10 22:17:06 +0200

    clear-cache added

--- nHentai/nHentai-cli.sh
+++ nHentai/nHentai-cli.sh
@@ -2,6 +2,17 @@
 
 #agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
 
+while [[ "${1}" ]];do
+    case "${1}" in
+        -C|--clear-cache)
+            rm -rf $HOME/.cache/nHentai/*
+            exit 0
+            ;;
+    esac
+done
+
+
+
 
 printf "Enter Number: " && read num
 num=$(printf "$num" | cut -d\/ -f5)
